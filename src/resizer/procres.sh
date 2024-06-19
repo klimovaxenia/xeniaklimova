fc4c9c1c422c54c6313d3f863f84db815a1ea6e5
xenia <xenia@jubaba.local>
2024-06-19 13:57:10 +0400

    work

--- src/resizer/procres.sh
+++ src/resizer/procres.sh
@@ -4,11 +4,11 @@ S3_ROOT_DIR="xeniaklimova-library/public/p800-img"
 
 #SRC_DIR="full-size-img/graphics"
 #TGT_DIR="p800-img/graphics"
-SRC_DIR="full-size-img/prj-sculptures"
-TGT_DIR="p800-img/prj-sculptures"
+SRC_DIR="full-size-img/prj-likani"
+TGT_DIR="p800-img/prj-likani"
 
-for file in $SRC_DIR/*.jpg; do
-#for file in $SRC_DIR/*.png; do
+#for file in $SRC_DIR/*.jpg; do
+for file in $SRC_DIR/*.png; do
     #echo ${file##*/}
     FULL_SRC_PATH="${SRC_DIR}/${file##*/}"
     FULL_TGT_PATH="${TGT_DIR}/p800-${file##*/}"
--- src/resizer/uload.sh
+++ src/resizer/uload.sh
@@ -9,7 +9,8 @@ S3_ROOT_DIR="xeniaklimova-library/public/p800-img"
 #aws s3 cp ./p800-img/paintings-oils "s3://${S3_ROOT_DIR}/paintings-oils/"  --recursive
 #aws s3 cp ./p800-img/prj-association-3ds-model "s3://${S3_ROOT_DIR}/prj-association-3ds-model/"  --recursive
 #aws s3 cp ./p800-img/prj-birtvisi "s3://${S3_ROOT_DIR}/prj-birtvisi/"  --recursive
-aws s3 cp ./p800-img/prj-sculptures "s3://${S3_ROOT_DIR}/prj-sculptures/"  --recursive
+#aws s3 cp ./p800-img/prj-sculptures "s3://${S3_ROOT_DIR}/prj-sculptures/"  --recursive
+aws s3 cp ./p800-img/prj-likani "s3://${S3_ROOT_DIR}/prj-likani/"  --recursive
 #aws s3 cp ./p800-img/prj-gentelmen-club "s3://${S3_ROOT_DIR}/prj-gentelmen-club/"  --recursive
 #aws s3 cp ./p800-img/prj-jeger-house "s3://${S3_ROOT_DIR}/prj-jeger-house/"  --recursive
 #aws s3 cp ./p800-img/prj-lobster "s3://${S3_ROOT_DIR}/prj-lobster/"  --recursive
