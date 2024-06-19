bd7d1d1d229c6471e2a71cc4af1dd14e48649f0a
xenia <xenia@jubaba.local>
2024-06-19 13:05:39 +0400

    work

--- src/resizer/procres.sh
+++ src/resizer/procres.sh
@@ -4,11 +4,11 @@ S3_ROOT_DIR="xeniaklimova-library/public/p800-img"
 
 #SRC_DIR="full-size-img/graphics"
 #TGT_DIR="p800-img/graphics"
-SRC_DIR="full-size-img/prj-birtvisi"
-TGT_DIR="p800-img/prj-birtvisi"
+SRC_DIR="full-size-img/prj-sculptures"
+TGT_DIR="p800-img/prj-sculptures"
 
-#for file in $SRC_DIR/*.jpg; do
-for file in $SRC_DIR/*.png; do
+for file in $SRC_DIR/*.jpg; do
+#for file in $SRC_DIR/*.png; do
     #echo ${file##*/}
     FULL_SRC_PATH="${SRC_DIR}/${file##*/}"
     FULL_TGT_PATH="${TGT_DIR}/p800-${file##*/}"
