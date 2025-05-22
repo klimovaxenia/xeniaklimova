bd814572e034e7c580243eafbf6b14182c464b76
xenia <xenia@jubaba.fritz.box>
2025-05-22 15:12:37 +0400

    work

--- src/resizer/uload.sh
+++ src/resizer/uload.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 #S3_ROOT_DIR="xeniaklimova-library/public/p800-img"
-S3_ROOT_DIR="xeniaklimova-library/public/pictures"
+#S3_ROOT_DIR="xeniaklimova-library/public/pictures"
 #S3_ROOT_DIR="xeniaklimova-library/public/p1200-img"
-#S3_ROOT_DIR="xeniaklimova-library/public/p370-img"
+S3_ROOT_DIR="xeniaklimova-library/public/p370-img"
 
 #aws s3 cp ./p800-img/graphics "s3://${S3_ROOT_DIR}/graphics/"  --recursive
 #aws s3 cp ./p800-img/sketches "s3://${S3_ROOT_DIR}/sketches/"  --recursive
 #aws s3 cp ./p800-img/paintings-gouache "s3://${S3_ROOT_DIR}/paintings-gouache/"  --recursive
 #aws s3 cp ./p800-img/paintings-watercolors "s3://${S3_ROOT_DIR}/paintings-watercolors/"  --recursive
-#aws s3 cp ./p800-img/paintings-oils "s3://${S3_ROOT_DIR}/paintings-oils/"  --recursive
+aws s3 cp ./p370-img/paintings-oils "s3://${S3_ROOT_DIR}/paintings-oils/"  --recursive
 #aws s3 cp ./p800-img/prj-association-3ds-model "s3://${S3_ROOT_DIR}/prj-association-3ds-model/"  --recursive
 #aws s3 cp ./p800-img/prj-birtvisi "s3://${S3_ROOT_DIR}/prj-birtvisi/"  --recursive
 #aws s3 cp ./p800-img/prj-sculptures "s3://${S3_ROOT_DIR}/prj-sculptures/"  --recursive
@@ -27,7 +27,7 @@ S3_ROOT_DIR="xeniaklimova-library/public/pictures"
 #aws s3 cp ./p800-img/prj-three-models "s3://${S3_ROOT_DIR}/prj-three-models/"  --recursive
 #aws s3 cp ./p370-img/paintings-watercolors "s3://${S3_ROOT_DIR}/paintings-watercolors/"  --recursive
 #aws s3 cp ./full-size-img/paintings-watercolors "s3://${S3_ROOT_DIR}/paintings-watercolors/"  --recursive
-aws s3 cp ./full-size-img/prj-micro-home "s3://${S3_ROOT_DIR}/prj-micro-home/"  --recursive
+#aws s3 cp ./full-size-img/prj-micro-home "s3://${S3_ROOT_DIR}/prj-micro-home/"  --recursive
 
 
 #aws s3 cp ./full-size-img/paintings-watercolors/yellow-tulips.jpg "s3://xeniaklimova-library/public/pictures/paintings-watercolors/"
@@ -36,6 +36,6 @@ aws s3 cp ./full-size-img/prj-micro-home "s3://${S3_ROOT_DIR}/prj-micro-home/"  
 #aws s3 cp ./p370-img/paintings-watercolors/p370-yellow-tulips.jpg "s3://xeniaklimova-library/public/p370-img/paintings-watercolors/"
 #
 #aws s3 cp ./full-size-img/paintings-oils/2025-04-15-kirche.jpg "s3://xeniaklimova-library/public/pictures/paintings-oils/"
-#aws s3 cp ./p1200-img/paintings-oils/p1200-2025-04-15-kirche.jpg "s3://xeniaklimova-library/public/p1200-img/paintings-oils/"
-#aws s3 cp ./p800-img/paintings-oils/p800-2025-04-15-kirche.jpg "s3://xeniaklimova-library/public/p800-img/paintings-oils/"
-#aws s3 cp ./p370-img/paintings-oils/p370-2025-04-15-kirche.jpg "s3://xeniaklimova-library/public/p370-img/paintings-oils/"
+#aws s3 cp ./p1200-img/paintings-oils/p1200-watercolors-thumbnail.jpg "s3://xeniaklimova-library/public/p1200-img/paintings-oils/"
+#aws s3 cp ./p800-img/paintings-oils/p800-watercolors-thumbnail.jpg "s3://xeniaklimova-library/public/p800-img/paintings-oils/"
+#aws s3 cp ./p370-img/sketches/p370-sketches-thumbnail.jpg "s3://xeniaklimova-library/public/p370-img/sketches/"
